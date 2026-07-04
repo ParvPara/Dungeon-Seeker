4a4afcb6841cb4b903e62357de81796aba65be1b
Eric <ericrollering@gmail.com>
2026-07-04 15:19:08 +1200

    created a new encounter function that calls the take damage constructor to both the enemy and the player and added health getter constructors to both classes in main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,8 @@ public:
   Player(std::string name, int health)
       : playerName(name), playerHealth(health) {}
   void printStatus() { std::cout << playerName << playerHealth; }
-  void takeDamage(int Damage) {
-      playerHealth = playerHealth - Damage;
-    }
+  void takeDamage(int Damage) { playerHealth = playerHealth - Damage; }
+  int getHealth() const { return playerHealth; }
 };
 
 class Enemy {
@@ -21,20 +20,25 @@ private:
   std::string enemyType;
   int enemyHealth;
 
-  public:
-    Enemy(std::string type, int health) : enemyType(type), enemyHealth(health) {
+public:
+  Enemy(std::string type, int health) : enemyType(type), enemyHealth(health) {}
+  void takeDamage(int Damage) { enemyHealth = enemyHealth - Damage; }
 
-    }
-    void takeDamage(int Damage) {
-      enemyHealth = enemyHealth - Damage;
-    }
-
+  int getHealth() const { return enemyHealth; }
 };
+
+void encounter(Player &hero, Enemy &enemy) {
+  hero.takeDamage(10);
+  enemy.takeDamage(20);
+}
 
 int main() {
 
   Player hero("hero", 100);
   hero.printStatus();
+  Enemy enemy("Demon Lord", 100);
+  enemy.getHealth();
+  encounter(hero, enemy);
 
   return 0;
 }
